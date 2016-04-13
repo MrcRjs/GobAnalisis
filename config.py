e7e33a6b2a7b321321ff77b5e01001e2c45ba2d0
Marco A. Rojas <devMarcoRojas@gmail.com>
2016-04-12 19:26:31 -0500

    Configuración de Twitter API

---
+++ config.py
@@ -0,0 +1,6 @@
+twitter = dict(
+	consumer_key = 'App consumer key',
+	consumer_secret = 'App secret',
+	access_token = 'Account access token',
+	access_token_secret = 'Account access token secret'
+)
